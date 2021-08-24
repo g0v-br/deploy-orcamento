d14cac4a29db15d5cfe70cc893adfe2067bc2119
Gustavo Dias <gustavodias@uchicago.edu>
2021-08-23 22:11:38 -0300

    terms1

--- src/views/TermsAndConditions.vue
+++ src/views/TermsAndConditions.vue
@@ -5,7 +5,7 @@
   <img class="img-responsive lazy" src="g0v.png" alt="" style="float:left;margin:0px 10px"/>  
                     
     <p class="title">
-                          <p>Toda vez que você vir o número zero no lugar da letra o, lembre-se de que é possível usar a internet e o modo de pensar digital (a linguagem binária, 0 e 1) para mudar o “gov” tradicional. <br />
+                          <p><br />  <br />Toda vez que você vir o número zero no lugar da letra o, lembre-se de que é possível usar a internet e o modo de pensar digital (a linguagem binária, 0 e 1) para mudar o “gov” tradicional. <br />
 Inspirados pelo g0v original, de Taiwan, com o qual, porém, não possuímos nenhum vínculo oficial, somos um movimento político, mas sem partido ou ideologia; defendemos a transparência dos governos e buscamos soluções inteligentes que facilitem e incentivem a participação popular nos assuntos da política, provendo os cidadãos de informações e poder para moldar a sociedade.<br />
 O g0v.br é uma organização da sociedade civil que reúne pessoas dispostas a pôr a tecnologia a serviço do interesse público, desenvolvendo ferramentas que ampliem a capacidade dos brasileiros de monitorar as atividades realizadas pelo Estado -- em todas as suas esferas e poderes --, a fim de que, cada vez mais, o uso dos recursos públicos efetivamente atenda, e com eficiência, às necessidades e demandas da nação.
 É uma comunidade livre e descentralizada, feita de pessoas que chegam com ideias novas e encontram outras pessoas dispostas a pôr essas ideias em prática. Todos os nossos projetos utilizam softwares open source e são desenvolvidos com trabalho voluntário. É de cidadãos para o bem público. <br />
